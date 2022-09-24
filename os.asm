d7863336662bb001f5010cf93475771bc6036937
kutup tilkisi <89846189+kutuptilkisi@users.noreply.github.com>
2022-09-24 22:05:49 +0300

    Update os.asm

--- os.asm
+++ os.asm
@@ -1,10 +1,4 @@
-	BITS 16
-
-start:
-	mov a	BITS 16
-
-start:
-	mov ax, 07C0h		; Set up 4K sta [BITS 16]                                                  ; Işlemciye kod türü belirtilir
+ [BITS 16]                                                  ; Işlemciye kod türü belirtilir
            JMP SHORT Yukleyici_Basla
            NOP
  Yukleyici_Basla:
@@ -21,7 +15,7 @@ start:
            CALL ImlecTasi
            MOV si, Metin1
            CALL Yazdir
-           MOV dl, 5
+           MOV dl, 4
            MOV dh, 1
            CALL ImlecTasi
            MOV si, Metin2
@@ -56,39 +50,4 @@ start:
  Metin1               db               'YellowOS - v1.0', 0
  Metin2               db               'Merhaba', 0
  TIMES 510 - ($ - $$) db 0                         ; 510 byte tamamlanır  
- dw 0xAA55ck space after this bootloader
-	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
-	mov ss, ax
-	mov sp, 4096
-
-	mov ax, 07C0h		; Set data segment to where we're loaded
-	mov ds, ax
-
-
-	mov si, metin1	; Put string position into SI
-	mov si, metin2
-	call metin	; Call our string-printing routine
-
-	jmp $			; Jump here - infinite loop!
-
-
-	metin1 db 'YellowOS - v1.0', 0
-	metin2 db 'Merhaba', 0
-
-
-metin:			; Routine: output string in SI to screen
-	mov ah, 0Eh		; int 10h 'print char' function
-
-.repeat:
-	lodsb			; Get character from string
-	cmp al, 0
-	je .done		; If char is zero, end of string
-	int 10h			; Otherwise, print it
-	jmp .repeat
-
-.done:
-	ret
-
-
-	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
-	dw 0xAA55		; The standard PC boot signaturex, 07C0h
+ dw 0xAA55
